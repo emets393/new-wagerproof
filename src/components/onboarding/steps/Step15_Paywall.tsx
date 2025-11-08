b1de1f912244c5aae49fc88a0dbffb39f03fa571
zukohabib <habib225@gmail.com>
2025-11-08 11:58:28 -0600

    Update Paywall component to display button for user interaction

--- src/components/onboarding/steps/Step15_Paywall.tsx
+++ src/components/onboarding/steps/Step15_Paywall.tsx
@@ -3,7 +3,7 @@ import type { PaywallHandle } from '@/components/Paywall';
 import { forwardRef } from 'react';
 
 export const Paywall = forwardRef<PaywallHandle>((props, ref) => {
-  return <PaywallComponent ref={ref} showButton={false} {...props} />;
+  return <PaywallComponent ref={ref} showButton={true} {...props} />;
 });
 
 Paywall.displayName = 'Paywall';
